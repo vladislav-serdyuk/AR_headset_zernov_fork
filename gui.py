66b0b22707986be8376c91ca878191bb984a4f1e
Zernov <90546939+ZernovTechno@users.noreply.github.com>
2024-07-06 12:37:44 +0500

    Update gui.py
    Add sphere-like interface
    Two dedicated images for eyes
    Settings

--- gui.py
+++ gui.py
@@ -1,19 +1,22 @@
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
-import time
 import applications.base_interface as base_interface
 import applications.calculator as calculator
 import applications.apps_panel as apps_panel
 import applications.clock as clock
 import applications.paint as paint
 import applications.keyboard as keyboard
+import applications.settings as settings
+import cv2
 
-right_panel = base_interface.right_panel(active=True)
 calc = calculator.calc(active=False)
-apps = apps_panel.panel(active=False)
 clocks = clock.pane(active=False)
 paint_app = paint.pane(active=False)
 keyboard_app = keyboard.pane(active=False)
+settings_app = settings.pane(active=False)
+
+right_panel = base_interface.right_panel(active=True)
+apps = apps_panel.panel(active=False)
 
 class gui_machine():
     messaging: bool =   True
@@ -45,23 +48,28 @@ class gui_machine():
         self.message = text
         self.message_color = color
 
-    def create_GUI (self, fingers): # Make an interface overlay
-        gui = Image.new('RGBA', (1480, 1440), (0,0,0,0))
+    def create_GUI (self, fingers, distance, xdegrees, config): # Make an interface overlay
+        gui = Image.new('RGBA', (5184, 5184), (0,0,0,0))
+        self.left_gui = Image.new('RGBA', (1440, 1440), (0,0,0,0))
+        self.right_gui = Image.new('RGBA', (1440, 1440), (0,0,0,0))
         self.draw = ImageDraw.Draw(gui)
+        if (xdegrees > 260):
+            xdegrees = xdegrees - 360
+        self.plus_pixels_by_x = int(xdegrees * 14.4)
 
         if (self.messaging):
-            if (self.message_iteration > 700):
+            if (self.message_iteration > 300):
                 self.messaging = False
                 self.message_iteration = 0
             else:
                 self.message_iteration += 1
-                self.draw.text(((1480//3), (50)),self.message,self.message_color,font=ImageFont.truetype("applications/resources/sans-serif.ttf", 45))
+                self.draw.text(((1480//2), (200)),self.message,self.message_color,font=ImageFont.truetype("applications/resources/sans-serif.ttf", 45))
                 
 
         if (len(fingers) > 20):
-            self.controller(fingers)
+            self.controller(fingers, distance, config)
             hand_image = Image.open('applications/resources/hand_icon.png')
-            gui.paste(hand_image, (1100, 72), hand_image)
+            gui.paste(hand_image, (1150, 172), hand_image)
 
         if (right_panel.active):
             right_panel_image = right_panel.main()
@@ -81,16 +89,26 @@ class gui_machine():
         if (keyboard_app.active):
             keyboard_image = keyboard_app.main()
             gui.paste(keyboard_image, keyboard_app.destination, keyboard_image)
+        if (settings_app.active):
+            settings_image = settings_app.main(config)
+            gui.paste(settings_image, settings_app.destination, settings_image)
+        self.calculated_x = int(config["Customize"]["to_interface_range"])//2
+        self.left_gui.paste(gui, (0+self.calculated_x-self.plus_pixels_by_x,0), gui)
+        self.right_gui.paste(gui, (0-self.calculated_x-self.plus_pixels_by_x,0), gui)
+        
+        if (eval(config["Options"]["barrel_distortion"])):
+            return cv2.cvtColor(np.array(self.left_gui), cv2.COLOR_BGRA2RGB), cv2.cvtColor(np.array(self.right_gui), cv2.COLOR_BGRA2RGB)
+        else:
+            return self.left_gui, self.right_gui
 
-        return gui
-
-    def controller(self, fingers): # Get fingers positions and check interface
+    def controller(self, fingers, distance, config): # Get fingers positions and check interface
 
         big_finger_coordinates = fingers[4] # Большой палец (координаты)
-        index_finger_coordinates = fingers[8] # Указательный палец (координаты)
+        index_finger_coordinates = fingers[int(config["Customize"]["index_finger"])] # Указательный палец (координаты)
         middle_finger_coordinates = fingers[12] # Средний палец (координаты) 
         ring_finger_coordinates = fingers[16] # Безымянный палец (координаты)
         pinky_finger_coordinates = fingers[20] # Мизинец (координаты)
+        index_finger_coordinates[1]-= self.calculated_x - self.plus_pixels_by_x
         #if (abs(big_finger_coordinates[1] - index_finger_coordinates[1]) < 60 and abs(big_finger_coordinates[2] - index_finger_coordinates[2]) < 60): # Check, if index finger near the big finger
         if (self.check_in_region(self.all_region_coordinates[0], self.all_region_coordinates[1], index_finger_coordinates)):
             
@@ -113,7 +131,7 @@ class gui_machine():
             self.middle_region = False
             self.left_region = False
 
-        if (abs(big_finger_coordinates[1] - index_finger_coordinates[1]) < 60 and abs(big_finger_coordinates[2] - index_finger_coordinates[2]) < 60): # Check, if index finger near the big finger
+        if (abs(big_finger_coordinates[1]-(self.calculated_x- self.plus_pixels_by_x) - index_finger_coordinates[1]) < 60 and abs(big_finger_coordinates[2] - index_finger_coordinates[2]) < 60): # Check, if index finger near the big finger
         # if index near big.
             if (self.check_in_region(calc.destination, [calc.destination[0] + calc.size[0], calc.destination[1] + calc.size[1]], index_finger_coordinates) and calc.active): # Check, if index finger inside the clocks
                 calc.destination = [index_finger_coordinates[1]-calc.size[0]//2, index_finger_coordinates[2]-calc.size[1]//2] # Set the center of clocks to the index finger
@@ -123,6 +141,8 @@ class gui_machine():
                 paint_app.destination = [index_finger_coordinates[1]-paint_app.size[0]//2, index_finger_coordinates[2] - paint_app.size[1] + 50] # Set the center of clocks to the index finger
             elif (self.check_in_region(keyboard_app.destination, [keyboard_app.destination[0] + keyboard_app.size[0], keyboard_app.destination[1] + keyboard_app.size[1]], index_finger_coordinates) and keyboard_app.active): # Check, if index finger inside the clocks
                 keyboard_app.destination = [index_finger_coordinates[1]-keyboard_app.size[0]//2, index_finger_coordinates[2] - keyboard_app.size[1]//2] # Set the center of clocks to the index finger
+            elif (self.check_in_region(settings_app.destination, [settings_app.destination[0] + settings_app.size[0], settings_app.destination[1] + settings_app.size[1]], index_finger_coordinates) and settings_app.active): # Check, if index finger inside the clocks
+                settings_app.destination = [index_finger_coordinates[1]-settings_app.size[0]//2, index_finger_coordinates[2] - settings_app.size[1]+50] # Set the center of clocks to the index finger
         
         elif (self.check_in_region(right_panel.destination, [right_panel.destination[0] + right_panel.size[0], right_panel.destination[1] + right_panel.size[1]], index_finger_coordinates) and right_panel.active): # Check, if index finger inside the clocks
                 self.command_receiver(right_panel.controller([index_finger_coordinates[1]-right_panel.destination[0], index_finger_coordinates[2]-right_panel.destination[1]]))
@@ -132,31 +152,35 @@ class gui_machine():
             self.command_receiver(paint_app.controller([index_finger_coordinates[1]-paint_app.destination[0], index_finger_coordinates[2]-paint_app.destination[1]]))
         elif (self.check_in_region(keyboard_app.destination, [keyboard_app.destination[0] + keyboard_app.size[0], keyboard_app.destination[1] + keyboard_app.size[1]], index_finger_coordinates) and keyboard_app.active): # Check, if index finger inside the clocks
             self.command_receiver(keyboard_app.controller([index_finger_coordinates[1]-keyboard_app.destination[0], index_finger_coordinates[2]-keyboard_app.destination[1]]))
+        elif (self.check_in_region(settings_app.destination, [settings_app.destination[0] + settings_app.size[0], settings_app.destination[1] + settings_app.size[1]], index_finger_coordinates) and settings_app.active): # Check, if index finger inside the clocks
+            self.command_receiver(settings_app.controller([index_finger_coordinates[1]-settings_app.destination[0], index_finger_coordinates[2]-settings_app.destination[1]], config))
 
 
     def command_receiver(self, command):
         match command:
             case "run_calc":
-                apps.active = not apps.active
                 calc.active = not calc.active
             case "run_appspanel":
                 apps.active = not apps.active
             case "run_keyboard":
-                apps.active = not apps.active
                 keyboard_app.active = not keyboard_app.active
             case "run_video":
                 self.create_message("Пока не работает", (255,0,0,255))
             case "run_compose":
-                apps.active = not apps.active
                 paint_app.active = not paint_app.active
             case "run_clock":
-                apps.active = not apps.active
                 clocks.active = not clocks.active
             case "run_computer":
                 self.create_message("Пока не работает", (255,0,0,255))
             case "run_folder":
                 self.create_message("Пока не работает", (255,0,0,255))
+            case "run_settings":
+                settings_app.active = not settings_app.active
+
             case "not_working":
                 self.create_message("Пока не работает", (255,0,0,255))
+            case "exit":
+                exit()
             case _:
                 pass
+
